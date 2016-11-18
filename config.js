af107be08c1e0d84e37a745091744d922c3421e4
Angel Salazar Marcatoma <Angel Salazar Marcatoma>
2016-11-17 19:56:34 -0600

    changing port to 8080

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "database" : "mongodb://angel:sesion1992@ds059306.mlab.com:59306/storagedb",
-  "port" : process.env.port || 3000,
+  "port" : process.env.port || 8080,
   "secretKey" : "mysupercoolsecretkey",
-  "algorithm" : "aes-256-ctr" 
+  "algorithm" : "aes-256-ctr"
 }
 // username : angel
 // password : sesion1992
