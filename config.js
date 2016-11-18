1667ed692684a6734ab9b58094e88744701f4129
Angel Salazar Marcatoma <Angel Salazar Marcatoma>
2016-11-17 20:14:20 -0600

    update PORT

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "database" : "mongodb://angel:sesion1992@ds059306.mlab.com:59306/storagedb",
-  "port" : process.env.port || 8080,
+  "port" : process.env.PORT || 8080,
   "secretKey" : "mysupercoolsecretkey",
   "algorithm" : "aes-256-ctr"
 }
